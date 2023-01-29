c49f831f31a42d60c917f8c4551985af6ca059e8
MathGuiMachDev <mathguimachdev@gmail.com>
2023-01-29 14:30:09 -0300

    Hystrix fault tolerance in hr-payroll and hr-worker

--- hr-payroll/src/main/java/com/mathguimachdev/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/mathguimachdev/hrpayroll/resources/PaymentResource.java
@@ -2,6 +2,7 @@ package com.mathguimachdev.hrpayroll.resources;
 
 import com.mathguimachdev.hrpayroll.entities.Payment;
 import com.mathguimachdev.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService serv;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlt")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable int days){
         Payment p = serv.getPayment(workerId,days);
+        return ResponseEntity.ok().body(p);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlt(long workerId, int days){
+        Payment p = new Payment("Alt",400.0,days);
         return ResponseEntity.ok().body(p);
     }
 }
